1f0588ddd5a4a42aaaa24994c672211446f6ead7
Hasan Altaf <hasanaltaf2001@gmail.com>
2020-08-26 18:07:12 -0700

    bug fix

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,30 @@
+import React from "react"
+import PropTypes from "prop-types"
+import styles from "./Layout.module.css"
+import NavBar from "../NavBar"
+import Footer from "../Footer"
+import Headroom from "react-headroom"
+
+const Layout = ({ children }) => {
+
+  return (
+    <>
+    <Headroom>
+    <NavBar />
+    </Headroom>
+    
+      <div className = {styles.elements}>
+        <main className = {styles.container}>{children}</main>
+      </div>
+        <footer>
+          <Footer />
+        </footer>
+    </>
+  )
+}
+
+// Layout.propTypes = {
+//   children: PropTypes.node.isRequired,
+// }
+
+export default Layout
