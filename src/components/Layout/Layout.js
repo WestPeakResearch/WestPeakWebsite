d00743ac6df574ef2601f754d7403a936af3ca79
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2023-04-16 23:58:35 -0400

    Re-add Layout component to fix github duplicate files

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,29 @@
+import React from "react"
+import styles from "./Layout.module.css"
+import NavBar from "../NavBar"
+import Footer from "../Footer"
+import Headroom from "react-headroom"
+
+const Layout = ({ children }) => {
+
+  return (
+    <>
+    <Headroom>
+    <NavBar />
+    </Headroom>
+    
+      <div className = {styles.elements}>
+        <main className = {styles.container}>{children}</main>
+      </div>
+        <footer>
+          <Footer />
+        </footer>
+    </>
+  )
+}
+
+// Layout.propTypes = {
+//   children: PropTypes.node.isRequired,
+// }
+
+export default Layout
