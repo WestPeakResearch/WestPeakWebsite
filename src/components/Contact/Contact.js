91fb51339b0c2683fb76ac5b4c3dfbfb2ca8e884
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2023-04-23 22:07:41 -0400

    Restyle contact page and remove custom dropdown component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,60 +1,153 @@
-import React from "react"
+import React, { useRef } from "react"
 import styles from "./Contact.module.css"
-import {Form, Container, Col, Row, Button} from "react-bootstrap"
+import { useFormik } from 'formik';
+import { InputText } from "primereact/inputtext";
+import { InputTextarea } from 'primereact/inputtextarea';    
+import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+import { classNames } from 'primereact/utils';
+import FacebookLogo from '../../images/facebook.svg';
+import LinkedinLogo from '../../images/linkedin.svg';
 
 
 function Contact(){
+  const toast = useRef(null);
 
-  return(
-    <>
-      <h2 className = {styles.header}>Please fill out the contact form below or email us at contact@westpeakresearch.com</h2>
-      <Container className={styles.container}>
-          <Form
-          name="contact"
-          method="post"
-          data-netlify="true"
-          onSubmit="submit"
-          >
-            <input type="hidden" name="form-name" value="contact" />
-            <Form.Row>
-              <Col lg = {6}>
-                <Form.Group className = {styles.name} controlId = "formBasicFirstName">
-                  <Form.Label>First Name </Form.Label>
-                  <Form.Control required size="sm" type = "text" name="first-name" />
-                </Form.Group>
-              </Col>
-              <Col lg = {6}>
-                <Form.Group className = {styles.name} controlId = "formBasicLastName">
-                  <Form.Label>Last Name </Form.Label>
-                  <Form.Control required size="sm" type = "text" name="last-name" />
-                </Form.Group>
-              </Col>
-            </Form.Row>
+  const showToast = () => {
+    toast.current.show({ severity: "success", summary: "Form Submitted", detail: "We'll get back to you as soon as possible!" });
+  };
 
-            <Row>
-              <Col lg = {6}>
-                <Form.Group className = {styles.email} controlId = "formBasicEmail">
-                  <Form.Label>Email Address </Form.Label>
-                  <Form.Control required size="sm" type = "email" placeholder="name@example.com" name = "email"/>
-                </Form.Group>
-              </Col>
-            </Row>
+  const formik = useFormik({
+    initialValues: {
+      first_name: '',
+      last_name: '',
+      email: '',
+      message: '',
+    },
+    validate: (data) => {
+      let errors = {};
 
-            <Row>
-              <Col lg = {6}>
-                <Form.Group className = {styles.textArea} controlId = "formBasicText">
-                  <h2>Type your message here</h2>
-                  <Form.Control required size="lg" as="textarea" type = "text" rows = "5" name="info"/>
-                </Form.Group>
-              </Col>
-            </Row>
-            
-            <div className= {styles.submit}>
-              <Button className = {styles.button} type="submit">Submit</Button>
-            </div>
-        </Form>
-      </Container>
-    </>             
+      if (!data.first_name) errors.first_name = 'First name is required.';
+      if (!data.last_name) errors.last_name = 'Last name is required.';
+      if (!data.email) errors.email = 'Email is required.';
+      if (!data.message) errors.message = 'Message is required.';
+
+      return errors;
+    },
+    onSubmit: (data) => {
+      data && showToast(data);
+      clearForm();
+    }
+  });
+
+  const isFormFieldInvalid = (name) => !!(formik.touched[name] && formik.errors[name]);
+
+  const onChange = (fieldName, event) => formik.setFieldValue(fieldName, event.target.value);
+
+  const clearForm = () => {
+    Object.keys(formik.values).map(key => {
+      formik.setFieldValue(key, '');
+      formik.setFieldTouched(key, false);
+    })
+  };
+
+  const getErrors = () => {
+    let error = false;
+
+    Object.keys(formik.values).map(key => error = isFormFieldInvalid(key));
+
+    return error ? <small className="p-error">All fields are required</small> : <small className="p-error">&nbsp;</small>;
+  }
+
+  return (
+    <div>
+      <h2 className={styles.header}>Please fill out the contact form below or email us at contact@westpeakresearch.com</h2>
+      <form 
+        className={styles.container}
+        name="contact" 
+        method="POST" 
+        data-netlify="true"
+        onSubmit={formik.handleSubmit} 
+      >
+        <Toast ref={toast} />
+
+        <div className={styles.infoContainer}>
+          <div className={styles.firstName}>
+            <span className="p-float-label">
+              <InputText
+                id="first-name"
+                name="first_name"
+                value={formik.values.first_name}
+                onChange={(e) => onChange('first_name', e)}
+                className={classNames({ 'p-invalid': isFormFieldInvalid('first_name') })}
+              />
+              <label id="first-name-label" htmlFor="first-name">First Name</label>
+            </span>
+          </div>
+
+          <div className={styles.lastName}>
+            <span className="p-float-label">
+              <InputText
+                id="last-name"
+                name="last_name"
+                value={formik.values.last_name}
+                onChange={(e) => onChange('last_name', e)}
+                className={classNames({ 'p-invalid': isFormFieldInvalid('last_name') })}
+              />
+              <label id="last-name-label" htmlFor="last-name">Last Name</label>
+            </span>
+          </div>
+
+          <div className={styles.email}>
+            <span className="p-float-label">
+              <InputText
+                id="email"
+                name="email"
+                value={formik.values.email}
+                onChange={(e) => onChange('email', e)}
+                className={classNames({ 'p-invalid': isFormFieldInvalid('email') })}
+              />
+              <label id="email-label" htmlFor="email">Email</label>
+            </span>
+          </div>
+        </div>
+
+        <div className={styles.textarea}>
+          <span className="p-float-label">
+            <InputTextarea
+              id="message"
+              name="message"
+              value={formik.values.message}
+              onChange={(e) => onChange('message', e)}
+              className={classNames({ 'p-invalid': isFormFieldInvalid('message') })}
+            />
+            <label id="message-label" htmlFor="message">Type your message here</label>
+          </span>
+        </div>
+
+        <div className={styles.error}>
+          {getErrors()}
+        </div>
+
+        <div className={styles.buttons}>
+          <Button className={styles.button} type="reset" label="Clear" onClick={clearForm} />
+          <Button className={styles.button + ' ' + styles.submit} type="submit" label="Submit" />
+        </div>
+
+        <div className={styles.socials}>
+          <h3>Make sure to follow our socials to stay up to date on events and hiring!</h3>
+          <div className={styles.socialsIcons}>
+            <a href="https://www.facebook.com/WestPeakResearch/" target="_blank">
+              <img src={FacebookLogo} alt="Facebook" />
+            </a>
+            <a href="https://linkedin.com/company/westpeak-research" target="_blank">
+              <img src={LinkedinLogo} alt="Linkedin" />
+            </a>
+          </div>
+        </div>
+      </form>
+    </div>
+    
   )
 }
 
