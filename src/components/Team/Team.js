3114c3a86b4dceb247ac356e128ac6563639f62e
Papershine <hilarylau119@gmail.com>
2023-07-07 19:58:40 -0700

    reindent file

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,87 +1,116 @@
-import React, {useState, useEffect} from "react"
-import { container, memberButtons, activeButton, inactiveButton, cards, mobileDropdownContainer, mobileDropdown, breakCard } from "./Team.module.css"
-import {useStaticQuery, graphql} from "gatsby"
+import React, { useState, useEffect } from "react"
+import {
+  container,
+  memberButtons,
+  activeButton,
+  inactiveButton,
+  cards,
+  mobileDropdownContainer,
+  mobileDropdown,
+  breakCard,
+} from "./Team.module.css"
+import { useStaticQuery, graphql } from "gatsby"
 import ManagementCard from "../ManagementCard"
 import AlumniTable from "../AlumniTable"
-import { Dropdown } from 'primereact/dropdown';
+import { Dropdown } from "primereact/dropdown"
 
-
-function Team(){
-  const data = useStaticQuery(graphql`query teamQuery {
-  allMarkdownRemark(
-    filter: {frontmatter: {type: {eq: "team"}}}
-    sort: {frontmatter: {name: ASC}}
-  ) {
-    nodes {
-      frontmatter {
-        degree
-        management
-        name
-        position
-        research
-        headshot {
-          publicURL
-          childImageSharp {
-            gatsbyImageData(placeholder: BLURRED, quality: 70, layout: CONSTRAINED, width: 1200, height: 1800)
+function Team() {
+  const data = useStaticQuery(graphql`
+    query teamQuery {
+      allMarkdownRemark(
+        filter: { frontmatter: { type: { eq: "team" } } }
+        sort: { frontmatter: { name: ASC } }
+      ) {
+        nodes {
+          frontmatter {
+            degree
+            management
+            name
+            position
+            research
+            headshot {
+              publicURL
+              childImageSharp {
+                gatsbyImageData(
+                  placeholder: BLURRED
+                  quality: 70
+                  layout: CONSTRAINED
+                  width: 1200
+                  height: 1800
+                )
+              }
+            }
+          }
+          fields {
+            slug
           }
         }
       }
-      fields {
-        slug
-      }
     }
-  }
-}`)
- 
-  const [currTeam, setTeam] = useState("Management");
+  `)
+
+  const [currTeam, setTeam] = useState("Management")
   // const [teamMobileSelection, setTeamMobileSelection] = useState('Management');
   const teamMobileOptions = [
-    {label: 'Management', value: 'Management'},
-    {label: 'Team Leads', value: 'Team Leads'},
-    {label: 'Senior Analysts', value: 'Senior Analysts'},
-    {label: 'Junior Analysts', value: 'Junior Analysts'},
-    {label: 'Alumni', value: 'Alumni'},
-  ];
+    { label: "Management", value: "Management" },
+    { label: "Team Leads", value: "Team Leads" },
+    { label: "Senior Analysts", value: "Senior Analysts" },
+    { label: "Junior Analysts", value: "Junior Analysts" },
+    { label: "Alumni", value: "Alumni" },
+  ]
 
   const team = data.allMarkdownRemark.nodes
-  
+
   useEffect(() => {
-    setTeam(localStorage.getItem('team') ? localStorage.getItem('team') : "Management")
+    setTeam(
+      localStorage.getItem("team") ? localStorage.getItem("team") : "Management"
+    )
   }, [])
 
   useEffect(() => {
-    window.addEventListener('beforeunload', function (e) {
-      e.preventDefault();
-      localStorage.setItem('team', "Management");
-    });
-  });
-   
+    window.addEventListener("beforeunload", function (e) {
+      e.preventDefault()
+      localStorage.setItem("team", "Management")
+    })
+  })
 
   const teamMembers = {
-    "Management": team.filter( member => member.frontmatter.management === "True"), 
-    "Team Leads": team.filter( member => member.frontmatter.management !== "True" && member.frontmatter.position.includes("Head")),
-    "Senior Analysts": team.filter( member => member.frontmatter.management !== "True" && member.frontmatter.position.includes("Senior Analyst")),
-    "Junior Analysts": team.filter( member => member.frontmatter.management !== "True" && member.frontmatter.position.includes("Junior Analyst")),
-    "Alumni": []
+    Management: team.filter(member => member.frontmatter.management === "True"),
+    "Team Leads": team.filter(
+      member =>
+        member.frontmatter.management !== "True" &&
+        member.frontmatter.position.includes("Head")
+    ),
+    "Senior Analysts": team.filter(
+      member =>
+        member.frontmatter.management !== "True" &&
+        member.frontmatter.position.includes("Senior Analyst")
+    ),
+    "Junior Analysts": team.filter(
+      member =>
+        member.frontmatter.management !== "True" &&
+        member.frontmatter.position.includes("Junior Analyst")
+    ),
+    Alumni: [],
   }
 
-  const handleTeamMobileSelect = (event) => {
-    event.preventDefault();
+  const handleTeamMobileSelect = event => {
+    event.preventDefault()
     setTeam(event.target.value)
-    localStorage.setItem('team', event.target.value);
-  };
- 
-  function handleMemberButtonClick(event){
-    setTeam(event.target.value)
-    localStorage.setItem('team', event.target.value);
+    localStorage.setItem("team", event.target.value)
   }
 
-  return(
+  function handleMemberButtonClick(event) {
+    setTeam(event.target.value)
+    localStorage.setItem("team", event.target.value)
+  }
+
+  return (
     <>
-      <main className = {container}>
+      <main className={container}>
         <div className={mobileDropdownContainer}>
           <span className="p-float-label">
-            <Dropdown 
+            <Dropdown
               value={currTeam}
               onChange={handleTeamMobileSelect}
               options={teamMobileOptions}
@@ -91,62 +120,92 @@ function Team(){
             />
           </span>
         </div>
-        
-        <div className = {memberButtons}>
-          {
-            Object.keys(teamMembers).map(key => (
-              <button  
-                onClick = {handleMemberButtonClick}
-                value = {key}
-                className = { key === currTeam ? activeButton : inactiveButton}
-              >
-                {key}
-              </button>
-            ))
-          }
+
+        <div className={memberButtons}>
+          {Object.keys(teamMembers).map(key => (
+            <button
+              onClick={handleMemberButtonClick}
+              value={key}
+              className={key === currTeam ? activeButton : inactiveButton}
+            >
+              {key}
+            </button>
+          ))}
         </div>
 
-        {currTeam === "Management" ? 
+        {currTeam === "Management" ? (
           <section className={cards}>
             <div className={breakCard}></div>
-            {teamMembers[currTeam].filter(member => member.frontmatter.position.includes('Co-Director')).map((member, index) => (
-              <>
-                <ManagementCard key = {index} member = {member.frontmatter} slug = {member.fields.slug}/>
-              </>
-            ))}
+            {teamMembers[currTeam]
+              .filter(member =>
+                member.frontmatter.position.includes("Co-Director")
+              )
+              .map((member, index) => (
+                <>
+                  <ManagementCard
+                    key={index}
+                    member={member.frontmatter}
+                    slug={member.fields.slug}
+                  />
+                </>
+              ))}
             <div className={breakCard}></div>
-            {teamMembers[currTeam].filter(member => member.frontmatter.position.includes('Internal')).map((member, index) => (
-              <>
-                <ManagementCard key = {index} member = {member.frontmatter} slug = {member.fields.slug}/>
-              </>
-            ))}
-            {teamMembers[currTeam].filter(member => member.frontmatter.position.includes('External')).map((member, index) => (
-              <>
-                <ManagementCard key = {index} member = {member.frontmatter} slug = {member.fields.slug}/>
-              </>
-            ))}
-            {teamMembers[currTeam].filter(member => member.frontmatter.position.includes('Tech')).map((member, index) => (
-              <>
-                <ManagementCard key = {index} member = {member.frontmatter} slug = {member.fields.slug}/>
-              </>
-            ))}
+            {teamMembers[currTeam]
+              .filter(member =>
+                member.frontmatter.position.includes("Internal")
+              )
+              .map((member, index) => (
+                <>
+                  <ManagementCard
+                    key={index}
+                    member={member.frontmatter}
+                    slug={member.fields.slug}
+                  />
+                </>
+              ))}
+            {teamMembers[currTeam]
+              .filter(member =>
+                member.frontmatter.position.includes("External")
+              )
+              .map((member, index) => (
+                <>
+                  <ManagementCard
+                    key={index}
+                    member={member.frontmatter}
+                    slug={member.fields.slug}
+                  />
+                </>
+              ))}
+            {teamMembers[currTeam]
+              .filter(member => member.frontmatter.position.includes("Tech"))
+              .map((member, index) => (
+                <>
+                  <ManagementCard
+                    key={index}
+                    member={member.frontmatter}
+                    slug={member.fields.slug}
+                  />
+                </>
+              ))}
           </section>
-          :
+        ) : (
           <section className={cards}>
             {teamMembers[currTeam].map((member, index) => (
               <>
-                <ManagementCard key = {index} member = {member.frontmatter} slug = {member.fields.slug}/>
+                <ManagementCard
+                  key={index}
+                  member={member.frontmatter}
+                  slug={member.fields.slug}
+                />
               </>
             ))}
           </section>
-        }
+        )}
 
-        {currTeam === "Alumni" ? <AlumniTable /> : null }
-      
+        {currTeam === "Alumni" ? <AlumniTable /> : null}
       </main>
     </>
   )
 }
-
 
 export default Team
