283d89890e7b8d85034432a96ca6c223f530ed99
cannonfodderalt <cannonfodderalt@gmail.com>
2025-04-06 20:53:29 -0700

    update placement table

--- src/components/CompanyPlacement/CompanyPlacement.tsx
+++ src/components/CompanyPlacement/CompanyPlacement.tsx
@@ -13,19 +13,41 @@ function CompanyPlacement() {
   const data: Queries.CompanyPlacementQuery = useStaticQuery(graphql`
     query CompanyPlacement {
       images: allFile(
-          filter: { absolutePath: { regex: "/placements\/.+(png|jpg|webp)/" }}
-          sort: { absolutePath: ASC }
-        ) {
+        filter: { absolutePath: { regex: "/placements/.+(png|jpg|webp)/" } }
+        sort: { absolutePath: ASC }
+      ) {
         nodes {
+          name
           childImageSharp {
             gatsbyImageData(placeholder: BLURRED, width: 1000)
           }
         }
       }
+      placements: allPlacementsJsonJson {
+        edges {
+          node {
+            title
+            images
+          }
+        }
+      }
     }
   `)
+  const imageMap: any = {}
+  for (const node of data.images.nodes) {
+    imageMap[node.name] = node.childImageSharp
+  }
 
-  const images = data.images.nodes.map((i) => getImage(i.childImageSharp)!)
+  const placementsEdges = data.placements.edges
+  const placementsWithImage = placementsEdges.map(edge => {
+    return {
+      title: edge.node.title,
+      images: edge.node.images?.map(image =>
+        image ? getImage(imageMap[image]) : null,
+      ),
+    }
+  })
+  console.log(placementsWithImage)
 
   return (
     <>
@@ -40,7 +62,9 @@ function CompanyPlacement() {
         </p>
       </div>
       <div className={imageContainer}>
-        <PlacementsTable images={images} />
+        {placementsWithImage.map(obj => (
+          <PlacementsTable title={obj.title || ""} images={obj.images || []} />
+        ))}
       </div>
     </>
   )
