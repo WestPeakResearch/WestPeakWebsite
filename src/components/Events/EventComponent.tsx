02623e3e0d5932617379326cac3e4bdb57be1d7e
Papershine <hilarylau119@gmail.com>
2025-08-25 21:21:29 -0400

    Add info sessions to events

--- src/components/Events/EventComponent.tsx
+++ src/components/Events/EventComponent.tsx
@@ -5,35 +5,51 @@ import {
   eventLinks,
   eventDetails,
   eventTitle,
-  closedButton
+  closedButton,
 } from "./Events.module.css"
 import LinkButton from "../ui/LinkButton/LinkButton"
-import { Button } from "primereact/button";
-import { DateTime } from "luxon";
+import { Button } from "primereact/button"
+import { DateTime } from "luxon"
 
-function EventComponent(props: {
-  event: any
-}) {
+function EventComponent(props: { event: any }) {
   const event = props.event
-  const today = DateTime.now().startOf("day").setZone('America/Vancouver')
-  const date = DateTime.fromISO(event.date!).setZone('America/Vancouver')
+  const today = DateTime.now().startOf("day").setZone("America/Vancouver")
+  const date = DateTime.fromISO(event.date!, {
+    zone: "America/Vancouver",
+  }).setZone("America/Vancouver")
 
   return (
     <div className={eventContainer}>
       <div className={eventDate}>
-        <span>{date.toFormat("LLLL dd")}</span>
+        <span>{date.toFormat("LLLL d")}</span>
         <span>{event.time}</span>
       </div>
       <div className={eventDetails}>
         <span className={eventTitle}>{event.title}</span>
         <span>{event.details}</span>
         <div className={eventLinks}>
-          <LinkButton link={event.link} target="_blank" text="&nbsp;Read More&nbsp;" />
-          {
-            today > DateTime.fromISO(event.deadline) ?
-            <Button className={closedButton} label="Registration Closed" outlined disabled/> :
-            <LinkButton link={event.registration} target="_blank" text="&nbsp;&nbsp;&nbsp;Sign Up&nbsp;&nbsp;&nbsp;&nbsp;" />
-          }
+          {event.link && (
+            <LinkButton
+              link={event.link}
+              target="_blank"
+              text="&nbsp;Read More&nbsp;"
+            />
+          )}
+          {event.registration &&
+            (today > DateTime.fromISO(event.deadline) ? (
+              <Button
+                className={closedButton}
+                label="Registration Closed"
+                outlined
+                disabled
+              />
+            ) : (
+              <LinkButton
+                link={event.registration}
+                target="_blank"
+                text="&nbsp;&nbsp;&nbsp;Sign Up&nbsp;&nbsp;&nbsp;&nbsp;"
+              />
+            ))}
         </div>
       </div>
     </div>
---
+++ src/content/Events/InfoSession2.md
@@ -0,0 +1,10 @@
+---
+type: "events"
+title: "Info Session 2"
+details: "Information Session for the WestPeak Research Association 2025 Analyst Hiring"
+date: "2025-09-12"
+time: "9:00pm PT"
+link: ""
+registration: ""
+deadline: "2025-09-11"
+---
