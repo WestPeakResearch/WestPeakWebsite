a58ed7d19838f75c908d227b4adf7f94d85d393b
cannonfodderalt <cannonfodderalt@gmail.com>
2025-09-01 17:53:50 -0700

    add extra text

--- src/components/Hiring/HiringOpen.tsx
+++ src/components/Hiring/HiringOpen.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import LinkButton from "../ui/LinkButton/LinkButton"
 import { StaticImage } from "gatsby-plugin-image"
 import { container, section, content, image, imageLeft } from "./Hiring.module.css"
+import FadeInBox from "../ui/FadeInBox/FadeInBox"
 
 function HiringOpen() {
   return (
@@ -26,7 +27,76 @@ function HiringOpen() {
         </div>
         <StaticImage alt="analysts" className={image} src="images/media_hiring_analysts.jpg" />
       </div>
+      <FadeInBox>
+        <div className={section}>
+          <StaticImage
+            alt="directors"
+            className={imageLeft}
+            src="images/media_hiring_directors.jpg"
+          />
+          <div className={content}>
+            <h3>Timeline</h3>
+            <ul>
+              <li>September 2: Information Session 1 Online, 7:00pm PT</li>
+              <li>September 2: Applications Open</li>
+              <li>September 12: Information Session 2 In Person, 7:00pm PT</li>
+              <li>September 18: Applications Close, 11:59pm PT</li>
+              <li>
+                September 20-21: Successful candidates will be contacted for
+                interviews
+              </li>
+            </ul>
+          </div>
+        </div>
+      </FadeInBox>
+      <FadeInBox>
+        <div className={section}>
+          <div className={content}>
+            <h3>WHAT WE LOOK FOR</h3>
+            <p>
+              We are looking for students with a high willingness to learn and
+              an interest in the capital markets. Our ideal students are driven
+              self-starters who are able to take the material they learn to the
+              next level. In the past, our most successful analysts have been
+              those with unwavering curiosity and eagerness to explore topics
+              related to financial markets. Students with an interest in
+              pursuing a career in the capital markets industry are encouraged
+              to apply.
+            </p>
+          </div>
+          <StaticImage
+            alt="discussion"
+            className={image}
+            src="images/media_hiring_discussion.jpg"
+          />
+        </div>
+      </FadeInBox>
+      <FadeInBox>
+        <div className={section}>
+          <StaticImage
+            alt="computer"
+            className={imageLeft}
+            src="images/media_hiring_computer.jpg"
+          />
+          <div className={content}>
+            <h3>WHAT WE OFFER</h3>
+            <p>
+              When you join WestPeak, you are joining a community of current
+              members and alumni that are as driven as you. You will have access
+              to exclusive training modules and resources designed to help you
+              learn finance concepts and develop skills needed within the
+              capital markets. Through your deliverables, you will gain valuable
+              research, analytical, and financial modelling skills. In addition
+              to the learning, you will also gain access to mentors and
+              networking sessions held with industry professionals and alumni
+              working in investment banking, sales and trading, asset
+              management, wealth management, and more.
+            </p>
+          </div>
+        </div>
+      </FadeInBox>
     </div>
+
   )
 }
 
