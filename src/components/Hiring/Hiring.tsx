14295005dfe57783a82581577322efc17a771ab9
cannonfodderalt <cannonfodderalt@gmail.com>
2025-09-01 17:47:30 -0700

    fix merge conflict

--- src/components/Hiring/Hiring.tsx
+++ src/components/Hiring/Hiring.tsx
@@ -11,7 +11,7 @@ enum HiringStatus {
   Closed = 'closed'
 }
 
-const HIRING_STATUS = HiringStatus.Upcoming as HiringStatus
+const HIRING_STATUS = HiringStatus.Open as HiringStatus
 
 function Hiring() {
   const coverDescription = getCoverDescription(HIRING_STATUS)
