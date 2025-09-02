81191971f2cfa207a41f23fd811b81cc2315b607
cannonfodderalt <cannonfodderalt@gmail.com>
2025-09-01 18:02:14 -0700

    add fadeinbox

--- src/components/Hiring/HiringOpen.tsx
+++ src/components/Hiring/HiringOpen.tsx
@@ -1,32 +1,51 @@
 import React from "react"
 import LinkButton from "../ui/LinkButton/LinkButton"
 import { StaticImage } from "gatsby-plugin-image"
-import { container, section, content, image, imageLeft } from "./Hiring.module.css"
+import {
+  container,
+  section,
+  content,
+  image,
+  imageLeft,
+} from "./Hiring.module.css"
 import FadeInBox from "../ui/FadeInBox/FadeInBox"
 
 function HiringOpen() {
   return (
     <div className={container}>
-      <div className={section}>
-        <div className={content}>
-          <h3>HOW TO APPLY</h3>
-          <p>
-            We offer a <b>Junior Analyst</b> program and a <b>Senior Analyst</b> program.
-            Please refer to our <a href="WPRA_Hiring_Package_2025.pdf" target="_blank"><b>Hiring Package </b></a>
-            for more details on each.
-          </p>
-          <p>
-            Please apply using the button below before <b>September 18th 2025, 11:59pm PT</b>.
-            Please note you must be a <b>UBC Vancouver student</b>, and may only apply to <b>one program</b>.
-          </p>
-          <p>
-            <LinkButton link="https://forms.gle/kMpGzKJRmLn6T7qJ7"
-              target="_blank"
-              text="Access the 2025 WestPeak Research Association Application Form" />
-          </p>
+      <FadeInBox>
+        <div className={section}>
+          <div className={content}>
+            <h3>HOW TO APPLY</h3>
+            <p>
+              We offer a <b>Junior Analyst</b> program and a{" "}
+              <b>Senior Analyst</b> program. Please refer to our{" "}
+              <a href="WPRA_Hiring_Package_2025.pdf" target="_blank">
+                <b>Hiring Package </b>
+              </a>
+              for more details on each.
+            </p>
+            <p>
+              Please apply using the button below before{" "}
+              <b>September 18th 2025, 11:59pm PT</b>. Please note you must be a{" "}
+              <b>UBC Vancouver student</b>, and may only apply to{" "}
+              <b>one program</b>.
+            </p>
+            <p>
+              <LinkButton
+                link="https://forms.gle/kMpGzKJRmLn6T7qJ7"
+                target="_blank"
+                text="Access the 2025 WestPeak Research Association Application Form"
+              />
+            </p>
+          </div>
+          <StaticImage
+            alt="analysts"
+            className={image}
+            src="images/media_hiring_analysts.jpg"
+          />
         </div>
-        <StaticImage alt="analysts" className={image} src="images/media_hiring_analysts.jpg" />
-      </div>
+      </FadeInBox>
       <FadeInBox>
         <div className={section}>
           <StaticImage
@@ -96,7 +115,6 @@ function HiringOpen() {
         </div>
       </FadeInBox>
     </div>
-
   )
 }
 
