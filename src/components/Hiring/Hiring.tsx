c0eb2553fd7c94f0c909eda9b83c2b78ad156196
cannonfodderalt <62614509+cannonfodderalt@users.noreply.github.com>
2026-04-07 14:55:56 -0700

    close hiring

--- src/components/Hiring/Hiring.tsx
+++ src/components/Hiring/Hiring.tsx
@@ -11,7 +11,7 @@ enum HiringStatus {
   Closed = 'closed'
 }
 
-const HIRING_STATUS: HiringStatus = HiringStatus.Open
+const HIRING_STATUS: HiringStatus = HiringStatus.Closed
 
 function Hiring() {
   const coverDescription = getCoverDescription(HIRING_STATUS)
